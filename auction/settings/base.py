847b27722f4c681fd84995ee85517d184053203f
rowsen2904 <bayrumovw@gmail.com>
2026-01-31 21:04:21 +0500

    add rest_framework_simplejwt

--- auction/settings/base.py
+++ auction/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party apps
     'rest_framework',
+    'rest_framework_simplejwt',
     # my apps
     'users'
 ]
@@ -106,6 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 # Internationalization
