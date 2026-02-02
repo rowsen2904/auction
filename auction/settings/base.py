7db5931af2559a653139c9f449cfda69608d6f37
rowsen2904 <bayrumovw@gmail.com>
2026-02-02 13:05:26 +0500

    add OTP verification email settings

--- auction/settings/base.py
+++ auction/settings/base.py
@@ -140,6 +140,19 @@ SPECTACULAR_SETTINGS = {
 }
 
 
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_TIMEOUT = 10
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+EMAIL_VERIFICATION_CODE_LENGTH = 6
+EMAIL_SEND_LIMIT = 60
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
