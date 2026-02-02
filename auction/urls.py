5ef0df25a94fd0ef648160eee7e5b0c2f9505b8f
rowsen2904 <bayrumovw@gmail.com>
2026-02-02 10:46:24 +0500

    api docs url changes

--- auction/urls.py
+++ auction/urls.py
@@ -29,8 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path('api/v1/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
 ]
 
 if settings.DEBUG:
