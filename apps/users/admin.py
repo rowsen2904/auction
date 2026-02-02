b9963d3abf49a50256c5d0dfc83c8323845b3a66
rowsen2904 <bayrumovw@gmail.com>
2026-02-02 12:13:53 +0500

    create user roles & Broker model

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,126 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from .models import Broker
+
+User = get_user_model()
+
+# Forms
+
+
+class UserCreationForm(forms.ModelForm):
+    """
+    User creation form for the admin (with password confirmation).
+    """
+    password1 = forms.CharField(
+        label=_("Password"), widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label=_("Password confirmation"), widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ("email", "first_name", "last_name", "role")
+
+    def clean_password2(self):
+        p1 = self.cleaned_data.get("password1")
+        p2 = self.cleaned_data.get("password2")
+        if p1 and p2 and p1 != p2:
+            raise forms.ValidationError(_("Passwords don't match."))
+        return p2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    """
+    User edit form for the admin.
+    The password is shown as a hashed readonly field.
+    """
+    password = ReadOnlyPasswordHashField(
+        label=_("Password"),
+        help_text=_(
+            "Raw passwords are not stored, so there is no way to see this user's password.")
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "role",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "groups",
+            "user_permissions",
+        )
+
+
+# Inlines
+class BrokerInline(admin.StackedInline):
+    model = Broker
+    extra = 0
+    can_delete = True
+    fields = ("is_verified", "verified_at", "verification_document")
+    readonly_fields = ("verified_at",)
+
+
+# Admins
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+
+    list_display = ("email", "first_name", "last_name", "role",
+                    "is_staff", "is_active", "date_joined")
+    list_filter = ("role", "is_staff", "is_active", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+    filter_horizontal = ("groups", "user_permissions")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "role")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff",
+         "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "role", "password1", "password2", "is_staff", "is_superuser", "is_active"),
+        }),
+    )
+
+    def get_inlines(self, request, obj=None):
+        """
+        Show BrokerInline only:
+        - when editing an existing user,
+        - and the user is a broker (by role or already has a broker profile).
+        """
+        if obj is None:
+            return []
+        if getattr(obj, "role", None) == getattr(User, "Roles").BROKER or hasattr(obj, "broker"):
+            return [BrokerInline]
+        return []
+
+
+@admin.register(Broker)
+class BrokerAdmin(admin.ModelAdmin):
+    list_display = ("user", "is_verified", "verified_at")
+    list_filter = ("is_verified",)
+    search_fields = ("user__email", "user__first_name", "user__last_name")
+    autocomplete_fields = ("user",)
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,19 @@
-from django.db import models
+import os
+from uuid import uuid4
+
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.core.exceptions import ValidationError
+from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+
+def broker_verification_document_folder(instance, filename):
+    _, ext = os.path.splitext(filename)
+    ext = ext.lower()
+
+    user_id = instance.user_id or "tmp"
+    return f"brokers/{user_id}/verification_documents/{uuid4().hex}{ext}"
 
 
 class UserManager(BaseUserManager):
@@ -27,6 +39,7 @@ class UserManager(BaseUserManager):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", User.Roles.ADMIN)
 
         if extra_fields.get("is_staff") is not True:
             raise ValueError("Superuser must have is_staff=True.")
@@ -37,10 +50,30 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
+    class Roles(models.TextChoices):
+        DEVELOPER = 'developer', _('Developer')
+        BROKER = 'broker', _('Broker')
+        ADMIN = 'admin', _('Admin')
+
     email = models.EmailField(_("email address"), unique=True)
 
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
-    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    first_name = models.CharField(
+        _("first name"),
+        max_length=150,
+        blank=True
+    )
+    last_name = models.CharField(
+        _("last name"),
+        max_length=150,
+        blank=True
+    )
+
+    role = models.CharField(
+        _("role"),
+        max_length=20,
+        choices=Roles.choices,
+        default=Roles.DEVELOPER
+    )
 
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
@@ -58,3 +91,47 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+    @property
+    def is_developer(self):
+        return self.role == self.Roles.DEVELOPER
+
+    @property
+    def is_broker(self):
+        return self.role == self.Roles.BROKER
+
+    @property
+    def is_admin(self):
+        return self.role == self.Roles.ADMIN
+
+
+class Broker(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='broker'
+    )
+
+    is_verified = models.BooleanField(default=False)
+    verified_at = models.DateTimeField(null=True, blank=True)
+    verification_document = models.FileField(
+        upload_to=broker_verification_document_folder,
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = _("broker")
+        verbose_name_plural = _("brokers")
+    
+    def __str__(self):
+        return '{}'.format(self.user.get_full_name())
+    
+    def save(self, *args, **kwargs):
+        if self.user.role != User.Roles.BROKER:
+            self.user.role = User.Roles.BROKER
+            self.user.save(update_fields=["role"])
+        super().save(*args, **kwargs)
