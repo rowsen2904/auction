bead73096efd0f349b467eda19b4892f4ff2237f
rowsen2904 <bayrumovw@gmail.com>
2026-02-03 19:43:10 +0500

    add FileSizeValidationMixin validator mixin

---
+++ helpers/validators.py
@@ -0,0 +1,16 @@
+from django.utils.translation import gettext_lazy as _
+from rest_framework import serializers
+
+
+class FileSizeValidationMixin:
+    MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+    def _validate_file_size(self, file, field_name: str):
+        if not file:
+            return file
+        if file.size > self.MAX_FILE_SIZE:
+            raise serializers.ValidationError(
+                _("File is too large (max 5MB)."),
+                code="file_too_large",
+            )
+        return file
