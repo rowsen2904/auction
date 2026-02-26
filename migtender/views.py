c8e6eb6eee2355477ff47e28068ca0c0aff77766
rowsen2904 <bayrumovw@gmail.com>
2026-02-26 11:20:27 +0500

    Update HealthCheckView to allow public access by setting permission_classes to AllowAny

--- migtender/views.py
+++ migtender/views.py
@@ -1,10 +1,13 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class HealthCheckView(APIView):
+    permission_classes = [AllowAny]
+
     @extend_schema(
         summary="Health check",
         description="Check if the server is running",
